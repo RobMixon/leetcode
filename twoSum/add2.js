b77562181b54e574a000538854ab32bd2e63e06b
Rob Mixon <mixonroberth@gmail.com>
2020-06-04 09:16:08 -0500

    got add2 to work correctly, need to work on speeding it up

--- twoSum/add2.js
+++ twoSum/add2.js
@@ -1,4 +1,4 @@
-// throwing codes on leetcode
+// had to change over to javascript and worked; need to speed up code; 
 
 [0,1,2,3,4,5];
 
@@ -8,9 +8,8 @@ function addTwoNumbers (nums, target) {
         for(let j=i + 1; j <nums.length; j++){
             if (nums[i] + nums[j] === target) {
                 console.log(i,j);
-                return[i,j]
+                return[i,j];
             }
         }
-    }
+    } return [];
 }
-addTwoNumbers([0,1,2,3,4],7);
